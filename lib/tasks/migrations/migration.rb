a96d9b45f37cb37060fd71d4316b16ddf7f78728
Max-Krieg666 <maxon_B@mail.ru>
2018-06-11 15:55:39 +0300

    minor fixes

--- lib/tasks/migrations/migration.rb
+++ lib/tasks/migrations/migration.rb
@@ -2,7 +2,7 @@ class CreateWatcherEvents < ActiveRecord::Migration
   def change
     create_table :watcher_events do |t|
       t.string :title,  null: false
-      t.string :status, default: 'Success'
+      t.string :status, default: 'success'
       t.string :kind
       t.text   :description
       t.text   :data
