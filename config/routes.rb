a0bf7ba45b308da5982e94d997127a0be4c79035
Max-Krieg666 <maxon_B@mail.ru>
2018-06-12 11:16:40 +0300

    add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :rails_watcher do
+        resources :events, only: [:index, :show]
         resources :statistics, only: [:index] do
           collection do
             get :daily
